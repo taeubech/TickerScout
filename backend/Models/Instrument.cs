22997e9198151c2d5c399666e2d8a4d1a385a55e
Christoph Taeubert <privat@toff.de>
2026-05-02 14:37:52 +0100

    Instrument.Symbol

--- backend/Models/Instrument.cs
+++ backend/Models/Instrument.cs
@@ -3,11 +3,13 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace TickerScout.Backend.Models;
 
-public sealed class Instrument
+public sealed record Instrument
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; init; }
+
+    public required string Symbol { get; init; }
 
     public required string Currency { get; init; }
 
